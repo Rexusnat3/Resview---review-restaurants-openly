ad6a304e01304de439bb9ba0dd140ef1fb49d626
Daniel R <daniel.r.casana@gmail.com>
2025-01-23 02:40:22 +0100

    Removed notation from code to be replaced with more natural notations.

--- restaurant_reviewer.py
+++ restaurant_reviewer.py
@@ -7,16 +7,7 @@ class RestaurantReviewer:
 
     def add_review(self, restaurant_name, reviewer_name, rating, comment):
         """
-        Adds a review to the system.
 
-        Args:
-            restaurant_name (str): The name of the restaurant.
-            reviewer_name (str): The name of the reviewer.
-            rating (int): Rating given by the reviewer (1-5).
-            comment (str): A comment about the restaurant.
-
-        Raises:
-            ValueError: If rating is not between 1 and 5.
         """
         if not (1 <= rating <= 5):
             raise ValueError("Rating must be between 1 and 5.")
@@ -32,25 +23,13 @@ class RestaurantReviewer:
 
     def get_reviews_for_restaurant(self, restaurant_name):
         """
-        Retrieves all reviews for a given restaurant.
 
-        Args:
-            restaurant_name (str): The name of the restaurant.
-
-        Returns:
-            list: A list of reviews for the restaurant.
         """
         return [review for review in self.reviews if review["restaurant_name"].lower() == restaurant_name.lower()]
 
     def get_average_rating(self, restaurant_name):
         """
-        Calculates the average rating for a given restaurant.
 
-        Args:
-            restaurant_name (str): The name of the restaurant.
-
-        Returns:
-            float: The average rating or None if there are no reviews.
         """
         restaurant_reviews = self.get_reviews_for_restaurant(restaurant_name)
         if not restaurant_reviews:
@@ -61,10 +40,7 @@ class RestaurantReviewer:
 
     def save_reviews_to_file(self, file_name):
         """
-        Saves all reviews to a JSON file.
 
-        Args:
-            file_name (str): The name of the file to save reviews to.
         """
         with open(file_name, 'w') as file:
             json.dump(self.reviews, file, indent=4)
@@ -72,10 +48,7 @@ class RestaurantReviewer:
 
     def load_reviews_from_file(self, file_name):
         """
-        Loads reviews from a JSON file.
 
-        Args:
-            file_name (str): The name of the file to load reviews from.
         """
         try:
             with open(file_name, 'r') as file:
@@ -93,10 +66,7 @@ class CategorizedRestaurantReviewer(RestaurantReviewer):
 
     def add_review(self, restaurant_name, reviewer_name, rating, comment, cuisine_type):
         """
-        Adds a review to the system with a cuisine type.
 
-        Args:
-            cuisine_type (str): The cuisine type of the restaurant.
         """
         super().add_review(restaurant_name, reviewer_name, rating, comment)
 
@@ -112,13 +82,7 @@ class CategorizedRestaurantReviewer(RestaurantReviewer):
 
     def get_reviews_by_cuisine(self, cuisine_type):
         """
-        Retrieves all reviews for a given cuisine type.
 
-        Args:
-            cuisine_type (str): The cuisine type.
-
-        Returns:
-            list: A list of reviews for the cuisine type.
         """
         return self.categorized_reviews.get(cuisine_type, [])
 
