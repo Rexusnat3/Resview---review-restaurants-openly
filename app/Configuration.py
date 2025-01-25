a50236e93ddd2571ac3c268bb0404da6ff1d1ce0
Daniel R <daniel.r.casana@gmail.com>
2025-01-25 02:44:20 +0100

    Corrected syntax and import errors

---
+++ app/Configuration.py
@@ -0,0 +1,5 @@
+SECRET_KEY = 'rexusnat3' #the secret key used by Flask for managingin a session
+MYSQL_HOST = 'localhost' #name of the MySQL server
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = '<PASSWORD>'
+MYSQL_DB = ''
